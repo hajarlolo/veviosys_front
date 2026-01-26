89c7f46456f513c9f2f10a18bb729903d79b9841
LOTFI Hajar <lotfihajarlotfihajar@gmail.com>
2026-01-26 16:30:42 +0100

    Rename Subscription to SubscriptionService in tests

--- src/app/services/subscription.spec.ts
+++ src/app/services/subscription.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Subscription } from './subscription';
+import { SubscriptionService } from './subscription';
 
-describe('Subscription', () => {
-  let service: Subscription;
+describe('SubscriptionService', () => {
+  let service: SubscriptionService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Subscription);
+    service = TestBed.inject(SubscriptionService);
   });
 
   it('should be created', () => {
